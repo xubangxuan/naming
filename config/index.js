b2e9b02adb9f16132629e30b0d1e83470df87f6c
xubangxuan <1120876214@qq.com>
2019-01-10 17:31:03 +0800

    nanmin  start

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '192.168.6.99', // can be overwritten by process.env.HOST
-    port: 9292, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: 'localhost', // can be overwritten by process.env.HOST
+    port: '8080', // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
